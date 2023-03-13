3caf937da5b1526460c1727b651282623bf88a12
bsuman <tsui.champ@gmail.com>
2023-03-13 08:38:42 +0100

    removed unnecessary std namespace

--- StackDS/StackDS/Stack.cpp
+++ StackDS/StackDS/Stack.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 Stack::Stack()
 {
-	cout << "Empty stack is created!" << std::endl;
+	cout << "Empty stack is created!" << endl;
 }
 
 Stack::~Stack()
 {
 	m_elements.erase(m_elements.begin(), m_elements.end());
-	cout << "All elements erased and stack is destroyed!" << std::endl;
+	cout << "All elements erased and stack is destroyed!" << endl;
 }
 
 int Stack::get_size() const {
@@ -19,18 +19,18 @@ int Stack::get_size() const {
 
 void Stack::push(int val) {
 	m_elements.insert(m_elements.begin(), val);
-	cout << "Element " << val << " pushed!" << std::endl;
+	cout << "Element " << val << " pushed!" << endl;
 }
 
 int Stack::pop() {
 	if (!m_elements.empty()) {
 		int val = m_elements.front();
 		m_elements.erase(m_elements.begin());
-		cout << "Element " << val << " popped!" << std::endl;
+		cout << "Element " << val << " popped!" <<endl;
 		return val;
 	}
 	else {
-		cout << "Stack is empty!" << std::endl;
+		cout << "Stack is empty!" << endl;
 	}
 }
 
